aab14774f1e2f9267ef48b3fcfeb9c3380d39414
waterbug <waterbug@126.com>
2025-04-29 11:42:53 +0800

    feat:完成day1

---
+++ basic_types.py
@@ -0,0 +1,21 @@
+a_int=-2
+a_float=3.14
+a_str="Wave"
+a_bool=True
+
+print(type(a_int),a_int)
+print(type(a_float),a_float)
+print(type(a_str),a_str)
+print(type(a_bool),a_bool)
+
+print("加法",a_int+a_float)
+print("乘法（字符串重复",a_str*3)
+print("比较：(a_int>5,a_str=='Wave'):",a_int>5,a_str=="Wave")
+
+"""
+下面是增加一些模除、幂、取整除
+"""
+print(10%3)
+print(a_str+a_str)
+print(2**5)
+print(7//2)         #地板除实际上是取整除法
