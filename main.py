4a5155fb84f84b641774425cdf8f1ec1f78e5ab8
waterbug <waterbug@126.com>
2025-05-02 21:13:52 +0800

    feat Day3的增加

---
+++ main.py
@@ -0,0 +1,7 @@
+
+from wave_utils import is_valid_impluse,fibonacci_ratio
+
+lengths=[1.0,1.7,1.2]
+print("有效驱动浪:",is_valid_impluse(lengths))
+print("斐波那奇关系:",fibonacci_ratio(lengths[0],lengths[1]))
+
---
+++ wave_utils.py
@@ -0,0 +1,9 @@
+def is_valid_impluse(lengths):
+    # 判断三浪是不是最短的浪
+    return lengths[1]>min(lengths[0],lengths[2])
+
+def fibonacci_ratio(a,b,target=1.618,tol=0.05):
+    #判断b是否约等于a*target
+    ratio=b/a if a!=0 else 0
+    return abs(ratio-target)<tol;
+
